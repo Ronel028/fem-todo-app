ead7172083c3ba262391eb92129a11e69d6f4611
Ronel <shujihanma028@gmail.com>
2022-04-03 07:09:56 -0700

    Updated code

--- script/app.js
+++ script/app.js
@@ -1,22 +1,38 @@
 
 let sunicon = document.querySelector('#sun');
 let moonIcon = document.querySelector('#moon')
+let toggleTheme = document.querySelector('.toggle');
+let complete = document.querySelector('#selectCompleted');
+let saveTheme = localStorage.getItem('theme');
+let saveComplete = localStorage.getItem('complete');
+// let iconSave = localStorage.getItem('icon');
 
-sunicon.addEventListener('click', function(){
-    document.body.classList.add('lightMode');
-    this.classList.add('hidden');
-    moonIcon.classList.remove('hidden');
-    moonIcon.classList.add('block');
+document.body.classList.add(saveTheme);
+complete.classList.add(saveComplete);
+
+if(!complete.classList.contains(saveComplete)){
+    complete.classList.add(saveComplete)
+}
+
+toggleTheme.addEventListener('click', (e)=>{
+
+    if(e.target.id === 'sun'){
+        document.body.classList.add('lightMode');
+        sunicon.classList.add('hidden');
+        moonIcon.classList.remove('hidden');
+        
+    
+        localStorage.setItem('theme', 'lightMode');
+    }
+    else if(e.target.id === 'moon'){
+        document.body.classList.remove('lightMode');
+        sunicon.classList.remove('hidden')
+        moonIcon.classList.add('hidden')
+
+        localStorage.removeItem('theme');
+
+    }
 })
-
-moonIcon.addEventListener('click', function(){
-    document.body.classList.remove('lightMode');
-    this.classList.add('hidden');
-    sunicon.classList.remove('hidden');
-    sunicon.classList.add('block');
-})
-
-
 
 
 // functions
@@ -44,7 +60,7 @@ let saveInput = (myInput, list, icount)=>{
     if(myInput.value === ""){
         return
     }
-    listItem.push(myInput.value);
+    listItem.push(myInput.value.trim());
     if(localStorage.getItem('Todo') === null){
         localStorage.setItem('Todo', JSON.stringify(listItem))
     }
@@ -52,21 +68,21 @@ let saveInput = (myInput, list, icount)=>{
         localStorage.setItem('Todo', JSON.stringify(listItem))
     }
     myInput.value = '';
-    list.innerHTML = displayTodo()
+    displayTodo(list)
     icount.innerHTML = todoListCount();
 }
 
 
 // this function display your data from local storage to UI
-let displayTodo = ()=>{
+let displayTodo = (list)=>{
      let output = '';
      listItem.forEach((item, index) =>{
          output += `
-                <li id="todoContain" class="borderlist flex justify-between items-center gap-6 border-b-[1px] px-[23px] py-[18px]">
+                <li class="borderlist flex justify-between items-center gap-6 border-b-[1px] px-[23px] py-[18px]">
                     <div class="group">
                         <input id="${index}" type="checkbox" >
                             
-                        <label for="${index}" class="LGB ml-[51px] relative flex-1 text-base font-JosefinSans font-normal capitalize cursor-pointer flex items-center justify-between">
+                        <label for="${index}" onClick="check(${index})" class="label LGB ml-[51px] relative flex-1 text-base font-JosefinSans font-normal capitalize cursor-pointer flex items-center justify-between">
                             ${item}
                         </label>
                     </div>
@@ -76,15 +92,18 @@ let displayTodo = ()=>{
                     </div>                            
                 </li>
              `
+                
      })
-     return output;
+     list.innerHTML = output;
 }
+
+
 
 // function to remove item todo
 function erase(index){
     listItem.splice(index, 1);
     localStorage.setItem('Todo', JSON.stringify(listItem))
-    dataContainer.innerHTML = displayTodo();
+    displayTodo(dataContainer);
 
 }
 
@@ -97,27 +116,80 @@ let todoListCount = ()=>{
     return count;
 }
 
-// function select(index){
-//     console.log('hello you click', index)
+function check(index){
 
-//     let completed = document.querySelector('#selectCompleted');
-//     let check = document.querySelector('input[type="checkbox"]:checked');
-//     let lis = document.querySelectorAll('#todoContain')
+    let labe = document.querySelectorAll('.label');
+    let listTodo = document.querySelectorAll('.borderlist');
+    let allTask = document.querySelector('#selectAll');
+    let activeTask = document.querySelector('#selectActive');
+    let clearComplete = document.querySelector('.clear-completed');
 
-//     completed.addEventListener('click', function(e){
+
+    listTodo[index].classList.add('activate');
+    labe[index].classList.toggle('label-line');
+    
+    // function for list all the complete task
+    complete.addEventListener('click', ()=>{
         
-//         console.log('hello', e.target.classList.contains('borderlist'));
-        
-//         if(check){
+        complete.classList.add('activeColor');
+        activeTask.classList.remove('activeColor');
+        allTask.classList.remove('activeColor')
+
+        listTodo.forEach(a=>{
+            if(!a.classList.contains('activate')){
+                a.style.display = 'none';
+            }
+            else if(a.classList.contains('activate')){
+                a.style.display = 'flex';
+            }
+        })
+
+        localStorage.setItem('complete', 'completed')
+
+    }) 
+    //------------------------------------------------------
+    
+    //function to list all task
+    allTask.addEventListener('click', ()=>{
+        complete.classList.remove('activeColor');
+        activeTask.classList.remove('activeColor');
+        allTask.classList.add('activeColor')
+        listTodo.forEach(listTask=>{
+             listTask.style.display = 'flex'
+        })
+    })
+    //-------------------------------------------------
+
+    // function for list all the incomplete task
+    activeTask.addEventListener('click', ()=>{
+        complete.classList.remove('activeColor');
+        activeTask.classList.add('activeColor');
+        allTask.classList.remove('activeColor')
+        listTodo.forEach(a=>{
+            if(a.classList.contains('activate')){
+                a.style.display = 'none';
+            }
+            else if(!a.classList.contains('activate')){
+                a.style.display = 'flex';
+            }
             
-//         }
+        })
+    })
+    //----------------------------------------------------------
+    clearComplete.addEventListener('click', ()=>{
         
-       
-   
-//     })
-// }
-
+        activeTask.classList.remove('activeColor');
+        allTask.classList.remove('activeColor');
+        complete.classList.remove('activeColor');
+            
+        listTodo.forEach(a=>{
+            if(a.classList.contains('activate')){
+                a.style.display = 'none';
+            }
+        })      
+    })
+}
 
 // call the function to display in UI
-dataContainer.innerHTML = displayTodo();
+displayTodo(dataContainer);
 itemCount.innerHTML = todoListCount();
